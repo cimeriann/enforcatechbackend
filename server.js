eadda0239a807a0837e90b45bb50bb2cd7d944a6
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2024-11-16 04:27:18 +0100

    server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDb from './config/db.js';
+import logger from './utils/logger.js';
+import errorHandler from './middleware/errorHandler.js';
+import authRouter from './routes/auth.js';
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+// connect to database
+(async () => {
+	await connectDb();
+  })();
+
+//middleware
+app.use(express.json());
+app.use(errorHandler)
+
+//routes
+app.get('/', (req, res) => {
+	res.send(`Enforcatech Backend API`);
+});
+app.use('/api/auth', authRouter);
+
+
+// start server
+app.listen(PORT, () =>{
+	logger.info(`Server started and running on http://localhost:${PORT}`);
+});
