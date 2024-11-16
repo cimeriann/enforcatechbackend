e3b3b87fe2c7c0fdf20719d4bb61ee4a9a7d2216
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2024-11-16 04:28:50 +0100

    auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+import { errorResponse } from "../utils/responses";
+import { isTokenValid } from "../utils/auth";
+import { StatusCodes } from "http-status-codes";
+
+const isLoggedIn = (res, req, next) => {
+	const authHeader = req.headers['authorization'];
+
+	if (!authHeader) {
+		return next(errorResponse(res, StatusCodes.UNAUTHORIZED, 'Access Denied, authorization header is required'));
+	}
+
+	const token = authHeader.split(' ')[1];
+
+	try {
+		const payload = isTokenValid(token);
+		req.user = { userId: payload.id};
+		next();
+	}catch(error){
+		return next(errorResponse(res, StatusCodes.UNAUTHORIZED, 'Access Denied, Invalid Token'));
+	}
+};
+
+export default isLoggedIn;
